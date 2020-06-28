42d0de84f00ba6165ff7852779ae202224ccfdcf
Oskari Lehtonen <lehtonen.osku@gmail.com>
2020-06-28 15:35:15 +0300

    tehtäviä

--- extra/asuntohakujquery.js
+++ extra/asuntohakujquery.js
@@ -3,11 +3,12 @@
     // call showHouses from done()-function
     $(document).ready(function(){
         $.ajax({
-            url: "asunnot.json",
+            url: "../asunnot.json",
             cache: false
         }).done(function(data) {
             console.log("done");
-            showFriends(data);        
+            console.log(data);
+			showHouses(data);
         }).fail(function() {
             console.log("error");
         }).always(function() {
@@ -20,17 +21,17 @@ function showHouses(data) {
     // loop through all houses data 
     $.each(data.talot, function(index, talo) {
         // create a div element and add "houseContainer" class to it
-        var div = $("#houses").append('<div class="taloContainer"><div>');
+        var div = $("<div class='taloContainer'><div>");
         // create img element and use "houseImage" class to it and src to house image
-        var image = $("#houses").append('<div class="taloContainer"><div>');
+        var image = $("<img class='taloImage' src="+talo.kuva+"/>");
         // create p element, use address as a text and "header" class
-        var header = $("#houses").append('<div class="taloContainer"><div>');
+        var header = $('<p class="otsikko">'+talo.osoite+'</p>');
         // create p element ja use house size as a text
-        var size = $("#houses").append('<div class="taloContainer"><div>');
+        var size = $('<p>'+talo.koko+'</p>');
         // create p element and use house text as a text and "text" class
-        var text = $("#houses").append('<div class="taloContainer"><div>');
+        var text = $('<p>'+talo.kuvaus+'</p>');
         // create p element and use house price as a text
-        var price = $("#houses").append('<div class="taloContainer"><div>');
+        var price = $('<p>'+talo.hinta+'</p>');
         //  add all elements to houseDiv lisÃ¤tÃ¤Ã¤n kaikki luodut elementit taloDiv-elementtiin
         div.append(image,header,size,text,price);
         // add div to #houses in DOM
